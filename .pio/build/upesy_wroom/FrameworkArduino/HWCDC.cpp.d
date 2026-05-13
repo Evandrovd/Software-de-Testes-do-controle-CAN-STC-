cef7537d394be03ac643b48ebad560a0d5444241
evandrovd <evandro2marinha@gmail.com>
2026-05-12 22:39:18 -0300

    0.1 - Funcional, muitos bugs concertados

---
+++ .pio/build/upesy_wroom/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/upesy_wroom/FrameworkArduino/HWCDC.cpp.o: \
+ /home/evandrovd/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ /home/evandrovd/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/evandrovd/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
---
+++ .pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.o: \
+ /home/evandrovd/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/evandrovd/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
